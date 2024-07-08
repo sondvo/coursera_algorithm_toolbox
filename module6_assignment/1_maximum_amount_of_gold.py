2a8915b9f65fda44631d878d2d323f747e5a585f
Son Vo <duyson1999@gmail.com>
2024-07-09 00:10:19 +0700

    module6, assignment1 _edited

--- module6_assignment/1_maximum_amount_of_gold.py
+++ module6_assignment/1_maximum_amount_of_gold.py
@@ -1,16 +1,21 @@
 import sys
 
 def max_weight(total_weight, arr):
-    n = len(arr)
-    T = [[0] * (total_weight + 1) for _ in range(n + 1)]
-    for i in range(1, n + 1):
-        for w in range(1, total_weight + 1):
-            T[i][w] = T[i - 1][w]
-            if w >= arr[i - 1]:
-                weight = T[i - 1][w - arr[i - 1]] + arr[i - 1]
-                if weight > T[i][w]:
-                    T[i][w] = weight
-    return T[n][total_weight]
+    optimal_weight = [
+        [0] * (total_weight + 1)
+        for _ in range(len(arr) + 1)
+    ]
+    for i in range(1, len(arr) + 1):
+        adding_weight = arr[i-1]
+        for w in range(1, total_weight+1):
+            optimal_weight[i][w] = optimal_weight[i-1][w]
+            if adding_weight <= w:
+                optimal_weight[i][w] = max(
+                    optimal_weight[i-1][w-adding_weight] + adding_weight,
+                    optimal_weight[i][w]
+                )
+        # print (optimal_weight)
+    return optimal_weight
 
 
 if __name__ == '__main__':
@@ -18,4 +23,4 @@ if __name__ == '__main__':
 	input_n = [list(map(int, i.split())) for i in input_n]
 	total_weight = input_n[0][0]
 	arr = sorted(input_n[1])
-	print(max_weight(total_weight, arr))
+	print(max_weight(total_weight, arr)[-1][-1])
